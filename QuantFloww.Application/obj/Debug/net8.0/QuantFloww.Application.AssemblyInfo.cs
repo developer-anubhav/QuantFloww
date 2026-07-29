5b09797fd6636512b5d88bba65c2486a2f0377b4
Anubhav <anubhavfordev24@gmail.com>
2026-07-29 16:43:25 +0530

    Portfolio Workstation Screen updates

--- QuantFloww.Application/obj/Debug/net8.0/QuantFloww.Application.AssemblyInfo.cs
+++ QuantFloww.Application/obj/Debug/net8.0/QuantFloww.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuantFloww.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d9f5a666db34321b908088e723d19696b688b60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c46b83928e110fda51677d6f5759e13956166cac")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuantFloww.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuantFloww.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
